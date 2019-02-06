cb58bd87cbdc30b3ac355383ea4ed2b24a4bbdd3
Mats Pfeiffer <mats.pfeiffer@googlemail.com>
2019-02-06 10:48:34 +0100

    Skip files without template literals.

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-let minify = require('html-minifier').minify;
-let getOptions = require('loader-utils').getOptions;
+const minify = require('html-minifier').minify;
+const getOptions = require('loader-utils').getOptions;
 
 module.exports = function(src) {
 
-  let str = src.match(/`(?:[^`\\]|\\.)*`/g,);
-  if (!str) {
-    throw new Error(`\n\n[minify-template-literal-loader] The template requires a template literal string (\`<template>\`)\n`);
-  }
+  let str = src.match(/`(?:[^`\\]|\\.)*`/g);
+  // return early to skip files without template literals
+  if (!str) return src
 
   // remove line breaks before grabbing export
   let removeLineBreak = src.replace(/[\n|\r]/gm, ' ');
   let exp = removeLineBreak.match(/^(.*?)`/g);
-  if (!exp) {
-    throw Error(`\n\n[minify-template-literal-loader] Unable to process export method.\n`)
-  }
+
+  if (!exp) throw Error(`\n\n[minify-template-literal-loader] Unable to process export method.\n`)
+
   let html = eval(str[0]);
   let result = minify(html, getOptions(this));
 
